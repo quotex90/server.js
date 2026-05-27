ac564aa84a104bd6d3c0380a27f7592bb5ba68b6
quotex90 <te877124@gmail.com>
2026-05-27 18:44:26 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("Quotex USA Running Successfully")
-})
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server Started")
-})
+  console.log(`Server running on port ${PORT}`);
+});
